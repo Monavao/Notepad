9084e59a971fd421173056de4b348f170906a2de
monavao <omid.monavari@hotmail.fr>
2023-05-08 19:04:32 +0200

    Mise en place du projet (classes + xml)

---
+++ app/src/main/java/com/example/notepad/Note.kt
@@ -0,0 +1,32 @@
+package com.example.notepad
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Note(var title: String? = "", var text: String? = "", var filename: String? = ""): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun describeContents(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        TODO("Not yet implemented")
+    }
+
+    companion object CREATOR : Parcelable.Creator<Note> {
+        override fun createFromParcel(parcel: Parcel): Note {
+            return Note(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Note?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
